abaf29900847f48413a5908eeb06332e099b74cc
Mark Downey <mark@thebakery.io>
2018-11-19 21:49:59 +0000

    mock raf

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,10 @@
+// mock client only functions to be ssr friendly
+if (window === undefined) {
+  requestAnimationFrame = function(callback) {
+    setTimeout(callback, 0);
+  };
+}
+
 export { default as Slide } from './components/slideshow/slide';
 export { default as Fade } from './components/slideshow/fade';
 export { default as Zoom } from './components/slideshow/zoom';
